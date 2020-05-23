313d67004e84833ce8a0a6d141136fd185cb8d87
Francisco <ffranciscocasas@gmail.com>
2020-05-23 12:29:12 -0300

    fran pete2

---
+++ public/javascripts/hamburger.js
@@ -0,0 +1,16 @@
+//Let's Create the JavaScript Part. 
+window.addEventListener("load", function() {
+const overlay = document.getElementById('overlay');
+const closeMenu = document.getElementById('close-menu');
+
+
+document.getElementById('open-menu') .addEventListener('click', function() {
+    overlay.classList.add('show-menu');
+});
+
+document.getElementById('close-menu').addEventListener('click', function(){
+    overlay.classList.remove('show-menu')
+});
+
+
+})
